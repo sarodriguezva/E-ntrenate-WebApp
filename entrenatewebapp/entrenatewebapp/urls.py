6bfb8d99770e1ad115fd3098a34b2f0317da71b1
Sebastián Bautista <jbautistas@unal.edu.co>
2021-12-10 13:15:39 -0500

    Integrado react y django

--- entrenatewebapp/entrenatewebapp/urls.py
+++ entrenatewebapp/entrenatewebapp/urls.py
@@ -1,7 +1,7 @@
 """entrenatewebapp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'))
 ]
